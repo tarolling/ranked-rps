c7e55663abd816865099e3085d20dad18d86f9ed
tarolling <benitojoens@gmail.com>
2024-08-18 18:22:30 -0500

    now?

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,9 @@ export default defineConfig(({ command, mode }): UserConfig => {
       // For example ['better-sqlite3'] if you use that in server functions.
       exclude: [],
     },
+    build: {
+      outDir: 'dist'
+    },
 
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
