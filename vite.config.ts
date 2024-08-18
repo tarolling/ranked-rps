ffcb352d8232eda01c2bb100e39074250fb5f537
tarolling <benitojoens@gmail.com>
2024-08-18 15:14:13 -0500

    add domain

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    base: '/ranked-rps/',
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
